8d49c273b701d869c4c59b2a4e9479897204a9d8
James Wu <ichangjwu@outlook.com>
2021-11-02 02:51:37 +0900

    The reset button also creates a new grid and able to draw on it

--- script.js
+++ script.js
@@ -22,55 +22,77 @@ for (i = 1; i <= 256; i++) {
 }
 
 // Need a reference to all square divs
-const squarediv = document.querySelectorAll("div.grid");
+function AddListeners() {
+  const squarediv = document.querySelectorAll("div.grid");
 
-// I have deviated from the project instructions.
-// Instead of drawing a sketch by hovering the cursor over the grid without clicking any button,
-// I chose a more natural approach. The user should be able to draw when they press and hold
-// their mouse button, and stop drawing when they release it.
-// For those features, we need the grid to listen to and act on two kinds of events:
-// 'mouseup' and 'mousedown'.
-squarediv.forEach(function (element) {
-  element.addEventListener('mousedown', OnMouseDown);
-  element.addEventListener('mouseup', OnMouseUp);
-});
+  // I have deviated from the project instructions.
+  // Instead of drawing a sketch by hovering the cursor over the grid without clicking any button,
+  // I chose a more natural approach. The user should be able to draw when they press and hold
+  // their mouse button, and stop drawing when they release it.
+  // For those features, we need the grid to listen to and act on two kinds of events:
+  // 'mouseup' and 'mousedown'.
+  squarediv.forEach(function (element) {
+    element.addEventListener('mousedown', OnMouseDown);
+    element.addEventListener('mouseup', OnMouseUp);
+  });
 
-// The function below is added primarily for the removeEventListener method, 
-// but it also cleans up the code.
-function ChangeColor(e) {
-  e.target.style.backgroundColor = "blue";
+  // The function below is added primarily for the removeEventListener method, 
+  // but it also cleans up the code.
+  function ChangeColor(e) {
+    e.target.style.backgroundColor = "blue";
+  }
+
+  // When the mouse button is pressed down, change the color at the target div.
+  // Then convert all other divs to listen to "mouseenter" event so 
+  // when the cursor moves over them, their color changes as well.
+  function OnMouseDown(e) {
+    e.target.style.backgroundColor = "blue";
+    squarediv.forEach(element => {
+      element.addEventListener("mouseenter", ChangeColor);
+    });
+  }
+
+  // We don't want to draw anymore when we release the mouse button.
+  // So when the button is released,
+  // stop changing color by removing the event listener from all divs.
+  function OnMouseUp() {
+    squarediv.forEach(element => {
+      element.removeEventListener('mouseenter', ChangeColor);
+    });
+  };
+
+  // The code below is added in case the mouse button is held and released outside of the grid.
+  // So we need a way to remove the 'mouseenter' event listener if the action above occurs.
+  // That is, remove the 'mouseenter' event listener when the click is released anywhere
+  // on the document.
+  document.addEventListener('mouseup', OnMouseUp);
 }
 
-// When the mouse button is pressed down, change the color at the target div.
-// Then convert all other divs to listen to "mouseenter" event so 
-// when the cursor moves over them, their color changes as well.
-function OnMouseDown(e) {
-  e.target.style.backgroundColor = "blue";
-  squarediv.forEach(element => {
-    element.addEventListener("mouseenter", ChangeColor);
-  });
-}
-
-// We don't want to draw anymore when we release the mouse button.
-// So when the button is released,
-// stop changing color by removing the event listener from all divs.
-function OnMouseUp() {
-  squarediv.forEach(element => {
-    element.removeEventListener('mouseenter', ChangeColor);
-  });
-};
-
-// The code below is added in case the mouse button is held and released outside of the grid.
-// So we need a way to remove the 'mouseenter' event listener if the action above occurs.
-// That is, remove the 'mouseenter' event listener when the click is released anywhere
-// on the document.
-document.addEventListener('mouseup', OnMouseUp);
+AddListeners();
 
 const button = document.querySelector("button");
+
 button.addEventListener('click', CreateNewGrid);
 
+
+
 function CreateNewGrid() {
+  const squarediv = document.querySelectorAll("div.grid");
   squarediv.forEach(element => {
     element.style.backgroundColor = "white";
   });
-};
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  };
+  let squaresperside = prompt("Enter the number of squares per side", "");
+
+  for (i = 1; i <= squaresperside ** 2; i++) {
+    let a = "div" + i;
+    a = document.createElement("div");
+    a.classList.add("grid");
+    container.appendChild(a);
+    let num = 100 / squaresperside;
+    a.style.cssText = `width: ${num}%`
+  }
+  AddListeners();
+}
